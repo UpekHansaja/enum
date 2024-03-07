2c36b6b2fb4adf142d95c60e97e3caf2b3e2f7f6
Upek Hansaja <upekhansajabk@gmail.com>
2024-03-07 23:51:48 +0530

    Enum for Beginners - Day-01

---
+++ A.java
@@ -0,0 +1,22 @@
+enum Name{
+    Upek,
+    Senuri,
+    Ayanaji,
+    Sudhara,
+    Oshan
+}
+
+class A{
+    public static void main(String[] args){
+        Name name = Name.Upek;
+        System.out.println(name);
+        Name name2 = Name.Senuri;
+        System.out.println(name2);
+        Name name3 = Name.Ayanaji;
+        System.out.println(name3);
+        Name name4 = Name.Sudhara;
+        System.out.println(name4);
+        Name name5 = Name.Oshan;
+        System.out.println(name5);
+    }
+}
